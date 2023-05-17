40f11b3e03b2eae4e6f9370034a7af47d647682e
AntonyAjay123 <ajaygeoffrey456@gmail.com>
2023-05-17 13:00:33 +0530

    add necessary reducers

--- src/components/routes/signin/signin.component.js
+++ src/components/routes/signin/signin.component.js
@@ -5,71 +5,94 @@ import { AuthContext } from "../../../context/auth.context";
 import { UserContext } from "../../../context/user.context";
 import { Navigate } from "react-router-dom";
 
-export default function SignIn(){
-    const [user,setUserDetails] = useState({
-        id:"",
-        name:"",
-        email:"",
-        password:"",
-        role:"",
-    })
-    const [error,setError]= useState("")
-    const [signedIn,setSignedIn]=useState(false);
-    const {auth,setAuth} = useContext(AuthContext);
-    const {curUser,setUser} = useContext(UserContext);
+export default function SignIn() {
+	const [user, setUserDetails] = useState({
+		id: "",
+		name: "",
+		email: "",
+		password: "",
+		role: "",
+	});
+	const [error, setError] = useState("");
+	const [signedIn, setSignedIn] = useState(false);
+	const { auth, setAuth } = useContext(AuthContext);
+	const { curUser, setUser } = useContext(UserContext);
 
-    //handling submit button
+	//handling submit button
 
-    function handleClick(event){
-        event.preventDefault();
-        const findUser = users.filter((dbUser)=>dbUser.email===user.email && dbUser.password===user.password);
-        if(findUser.length==0)
-        setError("Incorrect credentials")
-        else{
-            setAuth(true);
-            const foundUser=findUser[0];
-            setUser(()=>{
-                return {...foundUser}
-            });
-        }
-        }
+	function handleClick(event) {
+		event.preventDefault();
+		const findUser = users.filter(
+			(dbUser) =>
+				dbUser.email === user.email && dbUser.password === user.password
+		);
+		if (findUser.length == 0) setError("Incorrect credentials");
+		else {
+			setAuth(true);
+			const foundUser = findUser[0];
+			setUser(foundUser);
+		}
+	}
 
-    // ----Update user details entered by user
+	// ----Update user details entered by user
 
-    function handleChange(event){
-        const{name,value}=event.target;
-        setUserDetails((prev)=>{
-            return {
-                ...prev,
-                [name]:value
-            }
-        })
-    }
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setUserDetails((prev) => {
+			return {
+				...prev,
+				[name]: value,
+			};
+		});
+	}
 
-    return <div>
-    <h1></h1>
-    <div>
-    <form onSubmit={handleClick}>
-    <div>
-    <h4 style={{color:'red'}}>{error}</h4>
-    </div>
-  <div className="form-group">
-    <label htmlFor="exampleInputEmail1">Email address</label>
-    <input onChange={handleChange}  name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div className="form-group">
-    <label htmlFor="exampleInputPassword1">Password</label>
-    <input onChange={handleChange} name="password" type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-  </div>
-  <div className="form-check">
-    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-  </div>
-  <button  className="btn btn-primary">Submit</button>
-  {auth && <Navigate to="/" replace={true}/>}
-</form>
-    </div>
-    
-    </div>;
+	return (
+		<div>
+			<h1></h1>
+			<div>
+				<form onSubmit={handleClick}>
+					<div>
+						<h4 style={{ color: "red" }}>{error}</h4>
+					</div>
+					<div className="form-group">
+						<label htmlFor="exampleInputEmail1">Email address</label>
+						<input
+							onChange={handleChange}
+							name="email"
+							className="form-control"
+							id="exampleInputEmail1"
+							aria-describedby="emailHelp"
+							placeholder="Enter email"
+						/>
+						<small id="emailHelp" className="form-text text-muted">
+							We'll never share your email with anyone else.
+						</small>
+					</div>
+					<div className="form-group">
+						<label htmlFor="exampleInputPassword1">Password</label>
+						<input
+							onChange={handleChange}
+							name="password"
+							type="password"
+							className="form-control"
+							id="exampleInputPassword1"
+							placeholder="Password"
+						/>
+					</div>
+					<div className="form-check">
+						<input
+							type="checkbox"
+							className="form-check-input"
+							id="exampleCheck1"
+						/>
+						<label className="form-check-label" htmlFor="exampleCheck1">
+							Check me out
+						</label>
+					</div>
+					<button className="btn btn-primary">Submit</button>
+					{auth && <Navigate to="/" replace={true} />}
+				</form>
+			</div>
+		</div>
+	);
 }
--- src/context/user.context.js
+++ src/context/user.context.js
@@ -1,18 +1,49 @@
-import {createContext,useState} from "react"
+import { createContext, useState, useReducer } from "react";
 
-export const UserContext = createContext(
-    {
-        curUser:null,
-        setUser:()=>{}
-})
+export const UserContext = createContext({
+	curUser: null,
+	setUser: () => {},
+});
 
-export const UserProvider = ({children})=>{
-    const [curUser,setUser] = useState({
-        name:"",
-        role:"",
-        email:"",
-        password:""
-    })
-    const value={curUser,setUser};
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+const USER_ACTION_TYPES = {
+	SET_USER: "SET_USER",
+};
+const INITIAL_STATE = {
+	curUser: {
+		name: "",
+		role: "",
+		email: "",
+		password: "",
+	},
+};
+
+const userReducer = (state, action) => {
+	const { type, payload } = action;
+	console.log("user payload", payload);
+	switch (type) {
+		case USER_ACTION_TYPES.SET_USER:
+			return {
+				...state,
+				curUser: payload,
+			};
+		default:
+			throw new Error(`Unhandled type ${type}`);
+	}
+};
+export const UserProvider = ({ children }) => {
+	// const [curUser,setUser] = useState({
+	//     name:"",
+	//     role:"",
+	//     email:"",
+	//     password:""
+	// })
+	const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+	const { curUser } = state;
+
+	const setUser = (user) => {
+		console.log("user state", user);
+		dispatch({ type: USER_ACTION_TYPES.SET_USER, payload: user });
+	};
+	const value = { curUser, setUser };
+	return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
